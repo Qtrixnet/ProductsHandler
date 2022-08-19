20a854d3cb7252ce32bdfddbc64da01e09b6db77
Qtrixnet <qtrixnet@yandex.ru>
2022-08-19 16:50:04 +0600

    Revert "create request"
    This reverts commit cfeb2bc84332b994064ae714e3316be53fdad548.

--- src/scripts/utils/createChosenProductsArr.js
+++
@@ -1,7 +0,0 @@
-export default function createChosenProductsArr(products) {
-  return products.map(product => ({
-    is_available: true,
-    sku: `${product}`,
-    type: 'regular',
-  }))
-}
--- src/scripts/utils/createProductsArr.js
+++
@@ -1,8 +0,0 @@
-export default function createProductsArr(products) {
-  return products.map(product => ({
-    is_available: true,
-    quantity: 1,
-    sku: `${product}`,
-    type: 'regular',
-  }))
-}
