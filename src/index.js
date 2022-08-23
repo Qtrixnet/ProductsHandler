800d7a0d4d0459eadd9341e04223c30159062ada
Qtrixnet <qtrixnet@yandex.ru>
2022-08-23 11:51:43 +0600

    feat: create products handler

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './styles/index.scss';
+import './styles/base.scss';
+import './styles/product.scss';
+import productsHandler from './scripts/ProductsHandler';
+
+/* eslint-disable */
+
+if (typeof r46 === 'function') {
+  r46('recommend', 'd8dba85eb79c4173d9e81cc474b87ad8', {
+    items: [212497, 233858, 245237, 170386],
+  }, (response) => {
+   document.querySelector('#r46reco').append(productsHandler.removeParams(response.html));
+  }, () => {
+  });
+}
---
+++ src/scripts/ProductsHandler.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+
+class ProductsHandler {
+  constructor() {
+    this._code = null;
+  }
+
+  _parseProductsTemplate(string) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(string, 'text/html');
+
+    return doc.querySelector('.products');
+  }
+
+  removeParams(template) {
+    const products = this._parseProductsTemplate(template);
+
+    Array.from(products.children).forEach((link) => {
+      link.href = link.href.split('?')[0];
+      this._addClickHandler(link);
+    });
+
+    return products;
+  }
+
+  _addClickHandler(link) {
+    link.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      const target = evt.currentTarget;
+      const {sku} = target.dataset;
+
+      window.location.assign(`${target.href}?recommended_by=dynamic&recommended_code=${this._code}#sku=${sku}`);
+    });
+  }
+}
+
+const productsHandler = new ProductsHandler();
+
+export default productsHandler;
